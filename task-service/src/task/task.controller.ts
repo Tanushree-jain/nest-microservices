431198a71ee43ae15af065278466a5c9c0c157b1
Tanushree-jain <tanushreegangwal19@gmail.com>
2025-07-30 17:15:16 +0530

    task micro service logic complete

--- task-service/src/task/task.controller.ts
+++ task-service/src/task/task.controller.ts
@@ -1,4 +1,24 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task } from './task.model';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+    constructor(private readonly taskService: TaskService) { }
+    @MessagePattern('create_task')
+    createTask(@Payload() dto: CreateTaskDto): Task {
+        return this.taskService.createTask(dto);
+    }
+
+    @MessagePattern('get_tasks')
+    getTasks(): Task[] {
+        return this.taskService.getAllTasks();
+    }
+
+    @MessagePattern('delete_task')
+    deleteTask(@Payload() id: number): void {
+        this.taskService.deleteTask(id);
+    }
+}
--- task-service/src/task/task.service.ts
+++ task-service/src/task/task.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+    private tasks: Task[] = [];
+    private idCounter = 1;
+
+
+    createTask(dto: CreateTaskDto): Task {
+        const task: Task = {
+            id: this.idCounter++,
+            title: dto.title,
+            description: dto.description,
+        };
+        this.tasks.push(task);
+        return task;
+    }
+
+
+    getAllTasks(): Task[] {
+        return this.tasks;
+    }
+
+    deleteTask(id: number): void {
+        this.tasks = this.tasks.filter(task => task.id !== id);
+    }
+
+}
