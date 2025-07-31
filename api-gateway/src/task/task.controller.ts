ca4dcfda2ce5d535aa2ed8b21f57d8e24aa86481
Tanushree-jain <tanushreegangwal19@gmail.com>
2025-07-31 14:26:38 +0530

    rest files in api gateway

--- api-gateway/src/task/task.controller.ts
+++ api-gateway/src/task/task.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
 
-@Controller('task')
-export class TaskController {}
+@Controller('tasks')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  create(@Body() dto: CreateTaskDto) {
+    return this.taskService.createTask(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.taskService.getTasks();
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.taskService.deleteTask(id);
+  }
+}
--- api-gateway/src/task/task.service.ts
+++ api-gateway/src/task/task.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  constructor(@Inject('TASK_SERVICE') private client: ClientProxy) {}
+
+  async createTask(dto: CreateTaskDto) {
+    return this.client.send('create_task', dto);
+  }
+
+  async getTasks() {
+    return this.client.send('get_tasks', {});
+  }
+
+  async deleteTask(id: string) {
+    return this.client.send('delete_task', { id });
+  }
+}
