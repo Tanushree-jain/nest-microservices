22a5817dd35d744eed83a74d29b83b3d4b42a924
Tanushree-jain <tanushreegangwal19@gmail.com>
2025-07-31 14:17:12 +0530

    files creation in api gateway

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+ const app = await NestFactory.create(AppModule);
+
+  // Connect to Redis microservice
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.REDIS,
+    options: {
+      host: 'localhost',
+      port: 6379,
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000);
+  console.log('API Gateway is running on http://localhost:3000');
+
 }
 bootstrap();
